3c2a823e0badfdae429c5385d391dda754cd7126
codeMDG <rivoherizo2003@gmail.com>
2025-07-03 14:43:56 +0300

    solve string tokens

--- src/main/java/com/codemdg/strtokens/StringTokens.java
+++ src/main/java/com/codemdg/strtokens/StringTokens.java
@@ -1,15 +1,20 @@
 package com.codemdg.strtokens;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class StringTokens{
-    public List<String> getToken(String input){
-        String[] results = input.split("(\\W+)");
+public class StringTokens {
+    public List<String> getToken(String input) {
+        String[] results = input.trim().split("[^a-zA-Z0-9]|\\n|\\t|\\v|\\h|\\u0000");
         List<String> ret = new ArrayList<String>();
-        for(String result : results){
-            ret.add(result);
+        ret.add(0, "0");;
+        for (String result : results) {
+            if (!result.isEmpty()) {
+                ret.add(result);
+            }
         }
+        ret.set(0, String.valueOf(ret.size()-1));
 
         return ret;
     }
--- src/test/java/com/codemdg/strtokens/StringTokensTest.java
+++ src/test/java/com/codemdg/strtokens/StringTokensTest.java
@@ -6,32 +6,47 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 
 import org.junit.Test;
 
-import com.codemdg.dupliwords.DuplicateWordsRemover;
-
 public class StringTokensTest {
     @Test
-    public void testInput01() throws FileNotFoundException,IOException {
+    public void testInput03() throws FileNotFoundException, IOException {
         BufferedReader readerInput = new BufferedReader(
                 new FileReader(System.getProperty("user.dir") + "/src/test/java/com/codemdg/strtokens/input03.txt"));
         BufferedReader readerOutput = new BufferedReader(
                 new FileReader(System.getProperty("user.dir") + "/src/test/java/com/codemdg/strtokens/output03.txt"));
-
-        int q = Integer.parseInt(readerInput.readLine().trim());
-
-        String lineOutput, lineInput, lineInputDupliWordsRemoved;
-        DuplicateWordsRemover remover = new DuplicateWordsRemover();
-        for (int i = 1; i <= q; i++) {
-            lineInput = readerInput.readLine().trim();
-            lineOutput = readerOutput.readLine().trim();
-
-            lineInputDupliWordsRemoved = remover.removeDuplicateWords(lineInput);
-            assertEquals(lineOutput, lineInputDupliWordsRemoved);
+        StringTokens stringTokens = new StringTokens();
+        String lineInput = readerInput.readLine();
+        List<String> results = stringTokens.getToken(lineInput);
+        String line;
+        int i = 0;
+        while ((line = readerOutput.readLine()) != null) {
+            assertEquals(line, results.get(i));
+            i++;
         }
         readerOutput.close();
         readerInput.close();
-
     }
+
+    @Test
+    public void testInput04() throws FileNotFoundException, IOException {
+        BufferedReader readerInput = new BufferedReader(
+                new FileReader(System.getProperty("user.dir") + "/src/test/java/com/codemdg/strtokens/input04.txt"));
+        BufferedReader readerOutput = new BufferedReader(
+                new FileReader(System.getProperty("user.dir") + "/src/test/java/com/codemdg/strtokens/output04.txt"));
+        StringTokens stringTokens = new StringTokens();
+        String lineInput = readerInput.readLine();
+        List<String> results = stringTokens.getToken(lineInput);
+        String line;
+        int i = 0;
+        while ((line = readerOutput.readLine()) != null) {
+            assertEquals(line, results.get(i));
+            i++;
+        }
+        readerOutput.close();
+        readerInput.close();
+    }
+
 }
